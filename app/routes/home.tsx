9c2c0efb9dd693caaae359a5b5ff1cb878c01729
RitamMondal2468 <ritammondal700144@gmail.com>
2026-09-13 21:10:47 +0530

    feat: add initial ResumeLens interface

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeLens" },
+    { name: "description", content: "AI-powered resume analysis and insights for better job applications." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>See Your Resume Through an AI Lens</h1>
+          <h2>
+            Upload your resume and get AI-powered insights to improve your skills, content, and chances of landing your next opportunity.
+          </h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+  </main>
 }
